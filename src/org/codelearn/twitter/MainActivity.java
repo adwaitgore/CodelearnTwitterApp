e2cd65a75fc878e27d83dfb09736335aaac9afcb
Adwait Gore <adwait@codelearn.org>
2013-12-10 20:22:06 +0530

    Lesson 9 User Completion

--- src/org/codelearn/twitter/MainActivity.java
+++ src/org/codelearn/twitter/MainActivity.java
@@ -3,14 +3,32 @@ package org.codelearn.twitter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+
+
 import org.codelearn.twitter.R;
 
-public class MainActivity extends Activity {
+import android.widget.Button;
 
+public class MainActivity extends Activity {
+	Button _loginBtn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		_loginBtn = ( Button ) findViewById(R.id.btn_login);
+		 
+		_loginBtn.setOnClickListener(new View.OnClickListener() {
+		      @Override
+		      public void onClick(View v) {
+		        //This is a comment which does no good to your code. Feel free to remove it after you copy pasted this snippet.
+		        //Assignment - write logic to update the 'Login' text inside the button to 'Clicked'
+		    	  _loginBtn.setText("Clicked");
+		      }
+		  });
+		
+		
+		
 	}
 
 	@Override
