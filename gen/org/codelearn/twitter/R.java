53d57903fdfeb8145af08957065a80a4592379da
Adwait Gore <adwait@codelearn.org>
2013-12-10 16:05:15 +0530

    Lesson 7 User Completion

--- gen/org/codelearn/twitter/R.java
+++ gen/org/codelearn/twitter/R.java
@@ -24,9 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080008;
+        public static final int btn_login=0x7f080007;
+        public static final int fld_pwd=0x7f080006;
         public static final int fld_username=0x7f080003;
         public static final int header=0x7f080000;
+        public static final int pwd=0x7f080005;
+        public static final int pwd_block=0x7f080004;
         public static final int uname_block=0x7f080001;
         public static final int username=0x7f080002;
     }
@@ -40,7 +44,10 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int lbl_enter_pwd=0x7f050006;
         public static final int lbl_enter_username=0x7f050004;
+        public static final int lbl_login=0x7f050007;
+        public static final int lbl_pwd=0x7f050005;
         public static final int lbl_username=0x7f050003;
     }
     public static final class style {
