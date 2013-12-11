650ef0b6417c69b153ef7dfd7a13cc8b4d8e7fee
Adwait Gore <adwait@codelearn.org>
2013-12-11 11:19:47 +0530

    Lesson 11 User Completion

--- src/org/codelearn/twitter/TweetListActivity.java
+++ src/org/codelearn/twitter/TweetListActivity.java
@@ -3,13 +3,28 @@ package org.codelearn.twitter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class TweetListActivity extends Activity {
 
+	private ListView tweetListView;
+    private String[] stringArray ;
+    private ArrayAdapter tweetItemArrayAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tweet_list);
+		
+		stringArray = new String[10];
+        for(int i=0; i < stringArray.length; i++){
+            stringArray[i] = "String " + i;
+        }
+      tweetItemArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, stringArray);
+      tweetListView = (ListView) findViewById(R.id.tweetList);
+      tweetListView.setAdapter(tweetItemArrayAdapter);
 	}
 
 	@Override
