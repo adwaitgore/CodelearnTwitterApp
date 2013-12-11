a3da60591a9daa73994890dd2e0418d3bb0b8096
Adwait Gore <adwait@codelearn.org>
2013-12-11 19:33:33 +0530

    Lesson 15 User Completion Latest

--- gen/org/codelearn/twitter/R.java
+++ gen/org/codelearn/twitter/R.java
@@ -25,28 +25,33 @@ public final class R {
         public static final int user_profile=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000f;
         public static final int btn_login=0x7f080007;
         public static final int fld_pwd=0x7f080006;
         public static final int fld_username=0x7f080003;
         public static final int header=0x7f080000;
-        public static final int imageView1=0x7f080008;
+        public static final int imageView1=0x7f08000c;
         public static final int pwd=0x7f080005;
         public static final int pwd_block=0x7f080004;
-        public static final int textView2=0x7f08000a;
-        public static final int textView3=0x7f08000b;
+        public static final int textView2=0x7f08000d;
+        public static final int textView3=0x7f08000e;
+        public static final int tweetBody=0x7f08000a;
+        public static final int tweetDate=0x7f08000b;
+        public static final int tweetImage=0x7f080008;
         public static final int tweetTitle=0x7f080009;
         public static final int uname_block=0x7f080001;
         public static final int username=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_tweet_list=0x7f030001;
-        public static final int row_tweet=0x7f030002;
+        public static final int activity_tweet_detail=0x7f030001;
+        public static final int activity_tweet_list=0x7f030002;
+        public static final int row_tweet=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
-        public static final int tweet_list=0x7f070001;
+        public static final int tweet_detail=0x7f070001;
+        public static final int tweet_list=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -58,6 +63,7 @@ public final class R {
         public static final int lbl_pwd=0x7f050005;
         public static final int lbl_username=0x7f050003;
         public static final int no_tweet_found=0x7f050009;
+        public static final int title_activity_tweet_detail=0x7f05000a;
         public static final int title_activity_tweet_list=0x7f050008;
     }
     public static final class style {
