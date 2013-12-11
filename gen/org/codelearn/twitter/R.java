805e288b579ef320086f6e7b10b0c399f1290ce9
Adwait Gore <adwait@codelearn.org>
2013-12-11 11:19:47 +0530

    Lesson 11 User Completion

--- gen/org/codelearn/twitter/R.java
+++ gen/org/codelearn/twitter/R.java
@@ -24,13 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080009;
         public static final int btn_login=0x7f080007;
         public static final int fld_pwd=0x7f080006;
         public static final int fld_username=0x7f080003;
         public static final int header=0x7f080000;
         public static final int pwd=0x7f080005;
         public static final int pwd_block=0x7f080004;
+        public static final int tweetList=0x7f080008;
         public static final int uname_block=0x7f080001;
         public static final int username=0x7f080002;
     }
