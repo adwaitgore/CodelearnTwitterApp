04caf3c1cd9220112298f743b1607e1002365185
Adwait Gore <adwait@codelearn.org>
2013-12-11 14:30:22 +0530

    Lesson 12 User Completion

--- gen/org/codelearn/twitter/R.java
+++ gen/org/codelearn/twitter/R.java
@@ -22,15 +22,20 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int user_profile=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
+        public static final int action_settings=0x7f08000d;
         public static final int btn_login=0x7f080007;
         public static final int fld_pwd=0x7f080006;
         public static final int fld_username=0x7f080003;
         public static final int header=0x7f080000;
+        public static final int imageView1=0x7f08000c;
         public static final int pwd=0x7f080005;
         public static final int pwd_block=0x7f080004;
+        public static final int textView1=0x7f080009;
+        public static final int textView2=0x7f08000a;
+        public static final int textView3=0x7f08000b;
         public static final int tweetList=0x7f080008;
         public static final int uname_block=0x7f080001;
         public static final int username=0x7f080002;
@@ -38,6 +43,7 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_tweet_list=0x7f030001;
+        public static final int row_tweet=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
