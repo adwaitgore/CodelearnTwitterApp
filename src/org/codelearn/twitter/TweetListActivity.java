f395e7a5e326bf3c49a0907c3505006d6d8ee982
Adwait Gore <adwait@codelearn.org>
2013-12-12 12:26:32 +0530

    moved TweetAdapter class from private to public

--- src/org/codelearn/twitter/TweetListActivity.java
+++ src/org/codelearn/twitter/TweetListActivity.java
@@ -2,10 +2,7 @@ package org.codelearn.twitter;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,26 +21,9 @@ public class TweetListActivity extends Activity {
 		tweetItemArrayAdapter = new TweetAdapter(this, new String[10]);
 		
 		
-		
 		tweetListView = (ListView) findViewById(R.id.tweetList);
       tweetListView.setAdapter(tweetItemArrayAdapter);
 	}
-	
-	
-	private class TweetAdapter extends ArrayAdapter{
-	       private LayoutInflater inflater;
-	                
-	        public TweetAdapter(Activity activity, String[] items){
-	            super(activity, R.layout.row_tweet, items);
-	            inflater = activity.getWindow().getLayoutInflater();
-	        }
-	        
-	        @Override
-	        public View getView(int position, View convertView, ViewGroup parent){
-	            return inflater.inflate(R.layout.row_tweet, parent, false);
-	        }
-
-	   }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
