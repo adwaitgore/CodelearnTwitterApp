f303a471f27f4fd62d31b6e8d777ed3d2cd0bee3
Adwait Gore <adwait@codelearn.org>
2013-12-12 12:26:32 +0530

    fixing conflict

--- src/org/codelearn/twitter/TweetListActivity.java
+++ src/org/codelearn/twitter/TweetListActivity.java
@@ -6,7 +6,6 @@ import android.app.ListActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -26,25 +25,10 @@ public class TweetListActivity extends ListActivity {
 		tweetItemArrayAdapter = new TweetAdapter(this, new String[10]);
 		
 		
+
 		
 		setListAdapter(tweetItemArrayAdapter);
 	}
-	
-	
-	private class TweetAdapter extends ArrayAdapter{
-	       private LayoutInflater inflater;
-	                
-	        public TweetAdapter(Activity activity, String[] items){
-	            super(activity, R.layout.row_tweet, items);
-	            inflater = activity.getWindow().getLayoutInflater();
-	        }
-	        
-	        @Override
-	        public View getView(int position, View convertView, ViewGroup parent){
-	            return inflater.inflate(R.layout.row_tweet, parent, false);
-	        }
-
-	   }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
